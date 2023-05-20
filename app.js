e11f4a5a01e06eba87b3449550eeab3f8c689dee
migdracios <migdracios@gmail.com>
2023-05-20 16:37:17 +0900

    update insert Data

--- app.js
+++ app.js
@@ -28,17 +28,33 @@ const joinParty = async (date, name, color) => {
       name: name,
       color: color,
     });
-    console.log("Document written with ID: ", docRef.id);
+    alert(`${name}은(는) 보드게임 일정을 등록했다!`);
+    window.location.reload()
   } catch (e) {
     console.error("Error adding document: ", e);
   }
 };
 
+// 날짜 확인 함수
+const idList = ["월요일 점심","화요일 점심","수요일 점심","목요일 점심","금요일 점심","월요일 저녁","화요일 저녁","수요일 저녁","목요일 저녁","금요일 저녁"]
+const matchDate = (targetDate, matchingIndex=-1) => {
+  idList.forEach((date, index)=> {
+    if(targetDate==date) {
+      matchingIndex = index
+      // 반복문 실행 종료
+      return false
+    }
+  })
+
+  return matchingIndex
+}
+
 // 모달의 참가 버튼을 클릭하면 데이터 삽입 함수 호출
 $(".submit-btn").click(()=>{
   // 날짜, 이름, 컬러 값 찾아오기
   let date = $(".submit-li-date").text()
-  date = date.split(" ")[2] + date.split(" ")[3] + " " + date.split(" ")[5] // .submit-li-date 값이 : 05월 16일 수요일 점심 과 같이 되어 있음
+  date = date.split(" ")[2] + " " + date.split(" ")[3] // .submit-li-date 값이 : 05월 16일 수요일 점심 과 같이 되어 있음
+  date = matchDate(date)
   let name = $(".submit-input").val()
   let color = 0
   // ✔️ 표시가 들어있는지 확인해서 해당 div 집어내기
@@ -56,65 +72,29 @@ $(".submit-btn").click(()=>{
   }
 })
 
-// 날짜 확인 함수
-const matchDate = (targetDate, matchingIndex=-1) => {
-  // console.log(`input => ${targetDate}`)
-
-  dates.forEach((date, index)=> {
-    // console.log(`loop => ${date}`)
-    if(targetDate==date) {
-      matchingIndex = index
-      // 반복문 실행 종료
-      return false
-    }
-  })
-
-  return matchingIndex
-}
 // 데이터 조회 함수
 const getParties = async () => {
   const memberList = await getDocs(collection(db, "member"));
   $(".member-wrap").empty()
+  $(".party-on").text(0)
   memberList.forEach((doc) => {
     let data = doc.data()
     console.log(data)
+    let targetCard = $(`#${data.date}`)
 
-    $(".b-card-date").each(function() {
-      let htmlDate = $(this).text();
-      let dataDate = data.date.split(" ")[0] + " " + data.date.split(" ")[1];
-      
-      if (htmlDate === dataDate) {
-        let htmlCard = $(this).closest('.b-card');
-        let htmlDayDiv = htmlCard.find('.b-card-banner');
-        let htmlDayText = htmlDayDiv.text().split(" ")[1];
-        let dataDay = data.date.split(" ")[2];
-        
-        if (htmlDayText === dataDay) {
-          let htmlMember = htmlCard.find('.member-wrap');
-          let tempHtml = `<div class="b-card flex-col-between shadow-sm">
-                                <div class="b-card-banner flex-col-center text-w">월요일 점심</div>
-                                <div class="b-card-date mon">5월 15일</div>
-                                <h4>참여인원 <span class="party-on">3</span> /4</h4>
-                                <div class="member-wrap"></div>
-                                <button class="party-btn">이 일정 참가하기 🎲</button>
-                          </div>`;
-          htmlMember.append(tempHtml);
-        }
-      }
-    });
-    
-    // let targetDate = data.date.split(" ")[0] + " " + data.date.split(" ")[1]
-    // let result = matchDate(targetDate)
-    // if(result>=0 && result<5) {
-    //   // console.log('yay')
-    //   let targetClass = days[matchDate(targetDate)]
-    //   console.log(targetClass)
-    //   let targetParents = $(`.${targetClass}`).closest('.b-card').find('.member-wrap')
-    //   console.log(targetParents)
-    //   let targetChild = `<div class="member-icon color-${data.color}"></div>`
-      
-    
+    // 멤버 카운트 수 올리기
+    let memberCountDiv = targetCard.find('.party-on')
+    let memberCountNum = memberCountDiv.text()
+    memberCountDiv.text(memberCountNum * 1 + 1)
+
+    // 멤버 아이콘 추가하기
+    let colorIconDiv = `<button class="member-icon color-${data.color} data-toggle="popover" data-content="참여자 : ${data.name}"></button>`
+    targetCard.find(".member-wrap").append(colorIconDiv)
   })
 };
 
 getParties()
+
+$(document).ready(function() {
+  $('[data-toggle="popover"]').popover();
+});
--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-$(".b-modal-wrap").click(()=>{
-    $(".b-modal-wrap").hide()
-})
+$(".b-modal-wrap").click(() => {
+  $(".b-modal-wrap").hide();
+});
 
-$(".b-modal").click((event)=>{
-    event.stopPropagation()
-})
+$(".b-modal").click((event) => {
+  event.stopPropagation();
+});
 
+$(document).ready(function () {
+  $(
+    ".party-color-1, .party-color-2, .party-color-3, .party-color-4, .party-color-5"
+  ).click(function () {
+    let isChecked = $(this).text().includes("✔️");
 
-$(document).ready(function() {
-    $('.party-color-1, .party-color-2, .party-color-3, .party-color-4, .party-color-5').click(function() {
-        let isChecked = $(this).text().includes('✔️');
-        
-        $('.party-color-1, .party-color-2, .party-color-3, .party-color-4, .party-color-5').text('');
-        
-        if (!isChecked) {
-            $(this).text('✔️');
-        }
-    });
+    $(
+      ".party-color-1, .party-color-2, .party-color-3, .party-color-4, .party-color-5"
+    ).text("");
+
+    if (!isChecked) {
+      $(this).text("✔️");
+    }
+  });
 });
 
 // 요일 이름 배열
-let weekdays = ['일', '월', '화', '수', '목', '금', '토'];
+let weekdays = ["일", "월", "화", "수", "목", "금", "토"];
 
 // 오늘 날짜 가져오기
 let today = new Date();
@@ -30,24 +33,25 @@ let dates = [];
 for (let i = 1; i <= 5; i++) {
   let date = new Date(today);
   date.setDate(today.getDate() - today.getDay() + i);
-  let dateString = (date.getMonth() + 1) + "월 " + date.getDate() + "일";
+  let dateString = date.getMonth() + 1 + "월 " + date.getDate() + "일";
   dates.push(dateString);
 }
-// console.log(dates)
 
 // 결과 출력
-let days = ['mon','tue','wed','thu','fri']
-days.forEach((day, index)=>{
-    console.log(day, index)
-    $(`.${day}`).text(dates[index])
-})
+let days = ["mon", "tue", "wed", "thu", "fri"];
+days.forEach((day, index) => {
+  // console.log(day, index)
+  $(`.${day}`).text(dates[index]);
+});
 
-$(document).ready(function() {
-    $('.party-btn').click(function() {
-      let selectedDate = $(this).closest('.b-card').find('.b-card-date').text();
-      let selectedDay = $(this).closest('.b-card').find('.b-card-banner').text();
-      $(".b-modal-wrap").css("display", "flex")
-      $(".submit-li-date").text(selectedDate + " " + selectedDay) 
-    });
+$(document).ready(function () {
+  $(".party-btn").click(function () {
+    let selectedDate = $(this).closest(".b-card").find(".b-card-date").text();
+    let selectedDay = $(this).closest(".b-card").find(".b-card-banner").text();
+    $(".b-modal-wrap").css("display", "flex");
+    $(".submit-li-date").text(selectedDate + " " + selectedDay);
   });
+});
+
+
   
