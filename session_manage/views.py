2a7242b295a25c96f14a0f368529827d54bc6bbd
Emvegnomb <ys.hwang@teamsparta.co>
2023-09-21 13:58:30 +0900

    add v1 model setup

--- session_manage/views.py
+++ session_manage/views.py
@@ -1,4 +1,34 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Member
+from .forms import MemberForm
 
 def index(request):
-    return render(request, 'session_manage/index.html')
+    members = Member.objects.all()
+    return render(request, 'session_manage/index.html', {'members': members})
+
+def add_member(request):
+    if request.method == 'POST':
+        form = MemberForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = MemberForm()
+    return render(request, 'add_member.html', {'form': form})
+
+def edit_member(request, member_id):
+    member = Member.objects.get(pk=member_id)
+    if request.method == 'POST':
+        form = MemberForm(request.POST, instance=member)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = MemberForm(instance=member)
+    return render(request, 'edit_member.html', {'form': form, 'member': member})
+
+def delete_member(request, member_id):
+    member = Member.objects.get(pk=member_id)
+    member.delete()
+    return redirect('index')
